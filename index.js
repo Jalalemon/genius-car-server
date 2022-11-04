2191746af37144c5e50a9e8c4b837564cfbed7f7
Your Name <ju679935@gmail.com>
2022-11-05 01:03:49 +0600

    67 module

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 
 console.log(process.env.DB_USER);
 console.log(process.env.DB_PASSWORD);
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { useResolvedPath } = require('react-router-dom');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6fgntc0.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -17,6 +18,72 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+async function run() {
+    try{
+        const serviceCollection  = client.db('geniusCar').collection('services');
+        const orderCollection = client.db('geniusCar').collection('orders');
+
+
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor =serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+        app.get('/orders', async(req, res) => {
+            let query = {}; 
+
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders)
+        })
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        })
+
+        //orders api
+
+        app.post('/orders', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+
+        app.patch('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query ={_id: ObjectId(id)}
+            const updateDoc ={
+                $set:{
+                    status: status
+
+                }  
+            }
+            const result = await orderCollection.updateOne(query, updateDoc)
+            res.send(result)
+        })
+        app.delete('/orders/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result  = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(error => console.error(error))
 
 
 app.get('/', (req, res) => {
